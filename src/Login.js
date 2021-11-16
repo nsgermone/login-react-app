7026ee1e711fd994c7245e8a6fbcacc17260b0ca
ngermone <90794504+nsgermone@users.noreply.github.com>
2021-11-16 15:37:39 -0300

    Replace old components

--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,26 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+import { setUserSession } from './Utils/Common';
 
 function Login(props) {
     const username = useFormInput('');
     const password = useFormInput('');
     const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(null);
+    const [loading, setLoading] = useState(false);
 
     // handle button click of login form
    const handleLogin = () => {
-       props.history.push('Dashboard');
+       setError(null);
+       setLoading(true);
+       axios.post('http://localhost:4000/users/signin', { username: username.value, password: password.value}).then(response => {
+           setLoading(false);
+           setUserSession(response.data.token, response.data.user);
+           props.history.push('Dashboard');
+       }).catch(error => {
+           setLoading(false);
+           if (error.response.status === 401) setError(error.response.data.message);
+           else setError("Something went wrong. Please try again later");
+       });
    }
 
    return (
